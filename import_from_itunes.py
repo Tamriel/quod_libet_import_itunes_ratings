07790d61d0807f883ce49acbd605878fd28fad1b
Jan Korte <j.korte@me.com>
2016-05-05 01:38:09 +0200

    no computed ratings

--- import_from_itunes.py
+++ import_from_itunes.py
@@ -1,14 +1,16 @@
 # !/usr/bin/env python3
-# from pyItunes import *
+from pyItunes import *
 import pickle
 
 library = Library('iTunes Library.xml')
 ratings_dict = {}
 
 for _, song in library.songs.items():
-    if song.rating:
+    if song.rating and not song.rating_computed:
         key = song.artist + song.album + song.name
         ratings_dict[key] = song.rating
 
+
 with open('ratings', 'wb') as f:
     pickle.dump(ratings_dict, f, protocol=2)
+
